572c6f81791fde079ede961d8ba1221351b7c5cc
mrloop <ewan@mrloop.com>
2022-02-03 17:12:45 +0000

    Add modifier test

---
+++ test-app/tests/integration/modifiers/material-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Modifier | material', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<div {{material 'list'}}></div>`);
+
+    assert.ok(true);
+  });
+
+  test('it registers MDC class instance', async function (assert) {
+    assert.expect(2);
+    this.register = (inst) => {
+      assert.equal(inst.constructor.name, 'MDCList');
+    };
+    await render(hbs`<div {{material 'list' this.register}}></div>`);
+
+    assert.ok(true);
+  });
+});
